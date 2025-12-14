0be03e5049188f5c3d38b6a124896a4f34c1bf8c
MjLlanes <24-31014@g.batstate-u.edu.ph>
2025-12-14 14:22:25 +0800

    Update schema_v3.sql
    Create Table named Events

--- schema_v3.sql
+++ schema_v3.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE events (
+    event_id SERIAL PRIMARY KEY,
+    event_name VARCHAR(100) NOT NULL,
+    event_date DATE NOT NULL,
+    location_id INT,
+    description TEXT,
+    FOREIGN KEY (location_id) REFERENCES locations(location_id)
+);
