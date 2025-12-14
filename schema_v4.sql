0abfcc2a56e88fce9ee29c1631f3ad20f3ed62b6
MjLlanes <24-31014@g.batstate-u.edu.ph>
2025-12-14 14:26:39 +0800

    schema_v4.sql
    Create Table named Location

--- schema_v4.sql
+++ schema_v4.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE locations (
+    location_id SERIAL PRIMARY KEY,
+    location_name VARCHAR(100) NOT NULL,
+    address TEXT,
+    city VARCHAR(50),
+    state VARCHAR(50),
+    postal_code VARCHAR(20),
+    country VARCHAR(50)
+);
